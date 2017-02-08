ba80ead6b5db53f99d4c812a504a82eb7ce4d004
Vítor Plentz <vrplentz@inf.ufpel.edu.br>
2017-02-08 17:47:53 -0200

    Colocando barra esquerda

--- breakout.asm
+++ breakout.asm
@@ -11,8 +11,8 @@
 	screenHeight: 	.word 64
 	
 	#Colors
-	backgroundColor:.word	0x000000	 # black
-	borderColor:    .word	0xffffff	 # white	
+	backgroundColor:.word	0xffffff	 # white
+	borderColor:    .word	0xA9A9A9	 # grey	
 	blockColor: 	.word	0x2bcc2e	 # green
 	
 	#Informações da barra
@@ -22,16 +22,27 @@
 	main:
 	#Desenha o background
 	lw $a0, screenWidth #carrega a largura da tela
-	lw $a1, blockColor #carrega a cor do fundo
+	lw $a1, backgroundColor #carrega a cor do fundo
 	mul $a2, $a0, $a0 #faz o total do numero de pixels da tela 
 	mul $a2, $a2, 4 #endereços 
 	add $a2, $a2, $gp #add base of gp
 	add $a0, $gp, $zero #contador do loop
-	FillLoop:
+	FillBackgroundLoop:
 	beq $a0, $a2, Init #end condition, se a largura da tela for igual 
 	nop
 	sw $a1, 0($a0) #salva cor no background
 	addiu $a0, $a0, 4 #incrementa contador
-	j FillLoop
+	j FillBackgroundLoop
 	Init: #fim do desenho background
 	nop
+	add $a2, $zero, $gp #primeira posicao do display 
+	lw $a1,borderColor #carrega a cor da borda
+	li $a3, 16384 # ulitma posiçao da bora 
+	DrawBorder:
+	beq $a2, $a3, BorderDrawed
+	nop
+	sw $a1, 0($a2) #salva cor ($a1) no display ($a2)
+	addiu $a2, $a2, 256 #incrementa o display em 256 pois cada word tem 4 bits e o display 64
+	j DrawBorder
+	nop
+	BorderDrawed:nop
