a0b5ca33c3ac442ef84c092dd21b95223286c627
Vítor Plentz <vrplentz@inf.ufpel.edu.br>
2017-02-08 22:32:52 -0200

    Colocando outras barras

--- breakout.asm
+++ breakout.asm
@@ -27,22 +27,52 @@
 	mul $a2, $a2, 4 #endereços 
 	add $a2, $a2, $gp #add base of gp
 	add $a0, $gp, $zero #contador do loop
-	FillBackgroundLoop:
+	fillBackgroundLoop:
 	beq $a0, $a2, Init #end condition, se a largura da tela for igual 
 	nop
 	sw $a1, 0($a0) #salva cor no background
 	addiu $a0, $a0, 4 #incrementa contador
-	j FillBackgroundLoop
+	j fillBackgroundLoop
 	Init: #fim do desenho background
 	nop
 	add $a2, $zero, $gp #primeira posicao do display 
 	lw $a1,borderColor #carrega a cor da borda
-	li $a3, 16384 # ulitma posiçao da bora 
-	DrawBorder:
-	beq $a2, $a3, BorderDrawed
+	add $a3, $gp , 16384# ulitma posiçao da borda 
+	drawLeftBorder:
+	beq $a2, $a3, LeftBorderDrawed
 	nop
 	sw $a1, 0($a2) #salva cor ($a1) no display ($a2)
 	addiu $a2, $a2, 256 #incrementa o display em 256 pois cada word tem 4 bits e o display 64
-	j DrawBorder
+	j drawLeftBorder
 	nop
-	BorderDrawed:nop
+	LeftBorderDrawed:
+	
+	addi $a2, $gp, 252 #ultima coluna do display
+	add $a3, $gp , 16636# ulitma posiçao da borda 
+	drawRightBorder:
+	beq $a2, $a3, RightBorderDrawed
+	nop
+	sw $a1, 0($a2) #salva cor ($a1) no display ($a2)
+	addiu $a2, $a2, 256 #incrementa o display em 256 pois cada word tem 4 bits e o display 64
+	j drawRightBorder
+	nop
+	RightBorderDrawed: 
+	
+	addi $a2, $gp, 0 #primeira coluna do display
+	sw $a1, 0($a2) #salva cor ($a1) no display ($a2)
+	add $a3, $gp , 252# ulitma posiçao da borda 
+	drawTopBorder:
+	beq $a2, $a3, TopBorderDrawed
+	nop
+	sw $a1, 0($a2) #salva cor ($a1) no display ($a2)
+	addiu $a2, $a2, 4 #incrementa o display em 4 pois cada word tem 4 bits e o display 64
+	j drawTopBorder
+	nop
+	TopBorderDrawed: nop
+	
+	
+	
+	
+	
+	
+	
