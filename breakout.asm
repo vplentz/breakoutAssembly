ba560c068c4d91a0f043784a75da9021880442c3
Vítor Plentz <vrplentz@inf.ufpel.edu.br>
2017-03-02 01:14:25 -0300

    Ball created and moving down

--- breakout.asm
+++ breakout.asm
@@ -28,12 +28,17 @@
 	green: 	.word 0x44FF00 	# Green Bricks		#
 	blue: 	.word 0x0095FF	# Blue Bricks		#
 							#
-#########################################################
-	
+########## BALL ATRIBUTES ###############################
+	ballColor: .word 0xFFFFFF #white			#
+	position: .word 8320 #initialized with first position	#	
+	direction: .word 1	#initialized to go down		#
+###################################################
+
 .text
 	.globl main
 	main:
-	#Desenha o background
+	
+############### Desenha o background #########################
 	lw $a0, screenWidth #carrega a largura da tela
 	lw $a1, backgroundColor #carrega a cor do fundo
 	mul $a2, $a0, $a0 #faz o total do numero de pixels da tela 
@@ -118,6 +123,13 @@ j FillBackgroundLoop
 	jal buildPallet	
 	nop
 
+##########initialize ball ############################
+	lw $a0, position
+	add $a0, $a0, $gp
+	sw $a0, position
+	lw $a1, position # out of display range
+	jal drawBall
+	nop
 ################ move pallet ####################################################
 	li $t5, 1 # keep 1 to be used as a infinite looping
 	
@@ -145,9 +157,13 @@ j FillBackgroundLoop
 	
 	# $a0 to $a1 keep beggining and end of first paddle layer
 	# $t8 to $t9 keep beggining and end of last paddle layer
-	
+	# $a0 ball position
+	# $a1 ball direction
 	loop:
-	
+	lw $a0, position
+	lw $a1, direction
+	jal moveBall
+	nop
 	sw $zero, 0xFFFF0004
 	
 	jal getDir
@@ -157,7 +173,6 @@ j FillBackgroundLoop
 	nop
 	
 	bne $zero, $t5, loop
-	
 	
 	
 	
@@ -240,6 +255,7 @@ blueBricks:
 	jr $ra
 	nop
 
+
   #####################################
  #  Function that draws the pallet   #
 #####################################
@@ -310,5 +326,62 @@ getDir:
 	lw $t0, 0xFFFF0004
 	jr $ra
 	nop
-		
 
+  ####################################
+ #  Function that draws the ball   #############
+ #$a0 position to draw ####################
+# $a1 position to clean #################### 
+#####################################
+drawBall:
+	lw $t1, ballColor
+	lw $t2, backgroundColor
+	beq $a0, $a1, samePos
+	nop
+	sw $t1, 0($a0)
+	sw $t2, 0($a1) 
+	j endDrawBall
+	nop
+samePos:
+	sw $t1, 0($a0)
+endDrawBall:
+	jr $ra
+	nop
+######## move ball ###############
+# $a0 ball position #
+# $a1 direction#
+# $a1 = 0 = down #
+#####################################
+moveBall:
+	subi $sp, $sp, 4
+	sw $ra, 0($sp)
+	beq $a1, 1, down 
+	nop
+	j ballMoved
+	nop
+down: 	
+	move $a1, $a0 #set $a1 to clear position
+	addi $a0, $a0, 256 #set $a0 to draw position
+	sw $a0, position
+	jal delay
+	nop
+	jal drawBall
+	nop
+	j ballMoved
+	nop
+	
+ballMoved:
+	lw $ra, 0($sp)
+	addi $sp, $sp ,4
+	jr $ra
+	nop
+			
+############## DELAY#######
+delay: 	li $t1, 0
+delayLoop:	beq $t1, 250, endDelay
+		nop
+		addi $t1, $t1, 1
+		j delayLoop
+		nop
+endDelay: 	jr $ra
+		nop
+##########################
